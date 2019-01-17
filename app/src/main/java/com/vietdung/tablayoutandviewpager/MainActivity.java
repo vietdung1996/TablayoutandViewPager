98184d50c987bc5b65be6392249e757d9ac830d9
vietdung1996 <vietdungbk1996@gmail.com>
2019-01-17 08:53:01 +0700

    main_function

--- app/src/main/java/com/vietdung/tablayoutandviewpager/MainActivity.java
+++ app/src/main/java/com/vietdung/tablayoutandviewpager/MainActivity.java
@@ -1,13 +1,50 @@
 package com.vietdung.tablayoutandviewpager;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.vietdung.tablayoutandviewpager.fragment.FragmentOne;
+import com.vietdung.tablayoutandviewpager.fragment.FragmentThree;
+import com.vietdung.tablayoutandviewpager.fragment.FragmentTwo;
 
 public class MainActivity extends AppCompatActivity {
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+    private static final int PAGE_LIMIT = 3;
+    private static final int PAGE_ONE = 1;
+    private static final int PAGE_TWO = 2;
+    private static final int PAGE_THREE = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        setupViewPager();
+        mTabLayout.setupWithViewPager(mViewPager);
+        setupTabIcon();
+    }
+
+    private void setupViewPager() {
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager());
+        pagerAdapter.addFragment(new FragmentOne(), getString(R.string.title_first));
+        pagerAdapter.addFragment(new FragmentTwo(), getString(R.string.title_second));
+        pagerAdapter.addFragment(new FragmentThree(), getString(R.string.title_three));
+        mViewPager.setAdapter(pagerAdapter);
+        mViewPager.setOffscreenPageLimit(PAGE_LIMIT);
+    }
+
+    private void initView() {
+        mTabLayout = findViewById(R.id.tab_layout);
+        mViewPager = findViewById(R.id.view_pager);
+    }
+
+    public void setupTabIcon() {
+        mTabLayout.getTabAt(PAGE_ONE).setIcon(R.mipmap.ic_launcher_round);
+        mTabLayout.getTabAt(PAGE_TWO).setIcon(R.mipmap.ic_launcher_round);
+        mTabLayout.getTabAt(PAGE_THREE).setIcon(R.mipmap.ic_launcher_round);
+
     }
 }
---
+++ app/src/main/java/com/vietdung/tablayoutandviewpager/fragment/FragmentTwo.java
@@ -0,0 +1,22 @@
+package com.vietdung.tablayoutandviewpager.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.vietdung.tablayoutandviewpager.R;
+
+public class FragmentTwo extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_two, container, false);
+
+        return view;
+    }
+}
